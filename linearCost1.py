86ba84b1dcc202670ea8fd946c79246339afdcd0
DahyeYoon <dahye417@outlook.com>
2016-05-18 20:22:55 +0900

    5th commit

---
+++ linearCost1.py
@@ -0,0 +1,48 @@
+'''
+ # Created by DahyeYoon on 5/18/16 at 19:54
+ # Project Name: TensorflowTest2
+'''
+
+
+import tensorflow as tf
+from matplotlib import pyplot as plt
+
+# training data
+X = [1., 2., 3.]
+Y = [1., 2., 3.]
+m = n_samples =len(X)
+
+# weight = random value
+w = tf.placeholder(tf.float32)
+
+
+# H(x)=w*x
+hypothesis = tf.mul(X, w)
+
+# cost ==> sum{(H(x)-y<target value>)^2}/m : operation
+cost = tf.reduce_mean(tf.pow(hypothesis-Y, 2))/(m)
+
+
+# Variables(w, b) must be initialize before starting.
+init = tf.initialize_all_variables()
+
+# For graphs
+W_val = []
+cost_val = []
+
+# Launch the graph
+sess = tf.Session()
+sess.run(init)  # initialization run
+
+for i in range(-30, 50):
+    print i*0.1, sess.run(cost, feed_dict={w: i*0.1})
+    W_val.append(i*0.1)
+    cost_val.append(sess.run(cost, feed_dict={w: i*0.1}))
+
+
+# Graphic display
+plt.plot(W_val, cost_val, 'ro')
+plt.ylabel('Cost')
+plt.xlabel('W')
+plt.show()
+
--- linearCost2.py
+++ linearCost2.py
@@ -1,48 +1,43 @@
 '''
- # Created by DahyeYoon on 5/18/16 at 19:54
+ # Created by DahyeYoon on 5/18/16 at 19:42
  # Project Name: TensorflowTest2
 '''
 
 
 import tensorflow as tf
-from matplotlib import pyplot as plt
 
 # training data
-X = [1., 2., 3.]
-Y = [1., 2., 3.]
-m = n_samples =len(X)
+x_data = [1, 2, 3]
+y_data = [1, 2, 3]
 
 # weight = random value
-w = tf.placeholder(tf.float32)
+w = tf.Variable(tf.random_uniform([1], -10, 10))
 
+
+X = tf.placeholder(tf.float32)
+Y = tf.placeholder(tf.float32)
 
 # H(x)=w*x
-hypothesis = tf.mul(X, w)
-
+hypothesis = w*X
 # cost ==> sum{(H(x)-y<target value>)^2}/m : operation
-cost = tf.reduce_mean(tf.pow(hypothesis-Y, 2))/(m)
+cost = tf.reduce_mean(tf.square(hypothesis-Y))
+
+# Minimize
+descent = w- tf.mul(0.1, tf.reduce_mean(tf.mul((tf.mul(w, X)-Y), X)))
+update = w.assign(descent)
 
 
 # Variables(w, b) must be initialize before starting.
 init = tf.initialize_all_variables()
 
-# For graphs
-W_val = []
-cost_val = []
-
 # Launch the graph
 sess = tf.Session()
 sess.run(init)  # initialization run
 
-for i in range(-30, 50):
-    print i*0.1, sess.run(cost, feed_dict={w: i*0.1})
-    W_val.append(i*0.1)
-    cost_val.append(sess.run(cost, feed_dict={w: i*0.1}))
 
+#  Fitting the line
+for step in xrange(100):  # Repeat 20 times
+    sess.run(update, feed_dict={X:x_data, Y:y_data})  # cost minimization run
+    print step, sess.run(cost, feed_dict={X:x_data, Y:y_data}), sess.run(w)
 
-# Graphic display
-plt.plot(W_val, cost_val, 'ro')
-plt.ylabel('Cost')
-plt.xlabel('W')
-plt.show()
 
