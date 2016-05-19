3d8836fed4d69d92493a1e7803129d82c32fe2cc
DahyeYoon <dahye417@gmail.com>
2016-05-19 16:17:18 +0900

    11th commit

--- softmax.py
+++ softmax.py
@@ -27,7 +27,8 @@ learning_r = 0.001
 
 cost = tf.reduce_mean(-tf.reduce_sum(Y*tf.log(hypothesis), reduction_indices=1))
 
-optimizer = tf.train.GradientDescentOptimizer(learning_r).minimize(cost)
+optimizer = tf.train.GradientDescentOptimizer(learning_r)
+train = optimizer.minimize(cost)
 
 init = tf.initialize_all_variables()
 
@@ -36,6 +37,20 @@ with tf.Session() as sess:
     print sess.run(w)
 
     for step in xrange(2001):
-        sess.run(optimizer, feed_dict={X: x_data, Y: y_data})
+        sess.run(train, feed_dict={X: x_data, Y: y_data})
         if step % 200 == 0:
-            print step, sess.run(cost, feed_dict={X: x_data, Y: y_data})
+            print step, sess.run(cost, feed_dict={X: x_data, Y: y_data}), sess.run(w)
+
+            # test & one-hot encoding
+            a = sess.run(hypothesis, feed_dict={X: [[1, 11, 7]]})
+            print a, sess.run(tf.arg_max(a, 1))
+
+            b = sess.run(hypothesis, feed_dict={X: [[1, 3, 4]]})
+            print b, sess.run(tf.arg_max(b, 1))
+
+            c = sess.run(hypothesis, feed_dict={X: [[1, 1, 0]]})
+            print c, sess.run(tf.arg_max(c, 1))
+
+            all = sess.run(hypothesis, feed_dict={X: [[1, 11, 7], [1, 3, 4], [1, 1, 0]]})
+            print all, sess.run(tf.arg_max(all, 1))
+
